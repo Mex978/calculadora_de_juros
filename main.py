94428144b2045693f2ea1c6703f232351d36f25a
Max Lima <max.lima2@gmail.com>
2023-06-10 12:56:41 -0300

    fix: interest calculation

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ ANO = 12
 PERCENTUAL = 1 / 100
 CABECALHO = ["Mês", "Juros", "Total Investido", "Total Juros", "Montante"]
 
-VALOR_INICIAL = 500  # Valor incial. Ex: 500
-VALOR_MENSAL = 100  # Valor para depósito mensal. Ex: 100
+VALOR_INICIAL = 4000  # Valor incial. Ex: 500
+VALOR_MENSAL = 1000  # Valor para depósito mensal. Ex: 100
 ANOS = 10  # Quantidade de anos. Ex: 10
 TAXA_ANUAL = 10  # Valor da taxa anual. Ex: 10 -> 10%
 
@@ -19,25 +19,25 @@ def anual_para_mensal(taxa_anual):
 
 
 if __name__ == "__main__":
-    valor_inicial, deposito_mensal, meses = VALOR_INICIAL, VALOR_MENSAL, ANOS * ANO
+    montante, deposito_mensal, meses = VALOR_INICIAL, VALOR_MENSAL, ANOS * ANO
     taxa_anual = TAXA_ANUAL * PERCENTUAL
     taxa_mensal = anual_para_mensal(taxa_anual)
-    juros_total, investimento_total, tabela = 0, 0, [CABECALHO]
+    juros_total, investimento_total, tabela = 0, montante, [CABECALHO]
 
     for mes in range(meses):
-        valor_inicial, juros = (
-            valor_inicial + investimento_total,
-            valor_inicial * taxa_mensal,
-        )
+        juros = montante * taxa_mensal
+        montante += deposito_mensal + juros
+
         juros_total += juros
         investimento_total += deposito_mensal
+
         tabela.append(
             [
                 mes,
                 formatar_moeda(juros),
                 formatar_moeda(investimento_total),
                 formatar_moeda(juros_total),
-                formatar_moeda(valor_inicial + investimento_total),
+                formatar_moeda(montante),
             ]
         )
 
